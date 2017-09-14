6d4fdaf6bce7eddf3e12177f74669d4c0abf341c
Jase-Omeileo West <omeileo@icloud.com>
2017-09-14 08:13:32 -0500

    Added table that could be searched.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import Form from './Form';
+import SearchableTable from './SearchableTable';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Form />, document.getElementById('root'));
+ReactDOM.render(<SearchableTable />, document.getElementById('root'));
 registerServiceWorker();
