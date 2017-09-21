a9aabeab9aa9f09deb53184db0946be525ae38eb
Jase-Omeileo West <omeileo@icloud.com>
2017-09-21 13:49:59 -0500

    Allowed a submittion from one form to be used as input from another form, updating the form in real-time.

--- src/Forms_More.js
+++ src/Forms_More.js
@@ -5,28 +5,44 @@ import './semantic/dist/semantic.css';
 export default class FormsMore extends Component 
 {
     state = {
-        nameValue: "Kratos"
+        nameValue: "Kratos",
+        names: []
     };
+
+    // getInitialState()
+    // {
+    //     return { names: [] };
+    // }
 
     handleChange = (value) => {
         this.setState({
-            nameValue: value
+            nameValue: value,
         });
+    };
+
+    handleFormSubmit = (value) => {
+        this.setState({ names: this.state.names.concat(value) });
     };
 
     render()
     {
         return(
             <div>
+                <Form2 
+                    value = {this.nameValue} 
+                    onNameChange = {this.handleChange} 
+                    onSubmit = {this.handleFormSubmit}
+                />
+                <br/>
+
                 <Form1 
                     name = {this.state.nameValue}
                 />
                 <br/>
 
-                <Form2 
-                    value = {this.nameValue} 
-                    onNameChange = {this.handleChange} />
-                <br/><br/>
+                <Form3 
+                    names = {this.state.names}
+                />
             </div>
         );
     }
@@ -84,29 +100,56 @@ class Form2 extends Component
 
     handleFormSubmit = (event) => {
         event.preventDefault();
-        console.log(this.refs.name.value);
+        
+        if(this.refs.name.value !== '')
+        {
+            console.log(this.refs.name.value);
+            this.props.onSubmit(this.refs.name.value);
+            this.refs.name.value = '';
+        }
     };
     
     render()
     {
         return(
-            <div className="ui basic content center aligned segment">
-                <form onSubmit={this.handleFormSubmit} className='ui form'>
-                    
-                    <input
-                        type = 'text'    
-                        // value = {this.props.nameValue}
-                        placeholder = 'John'
-                        ref = 'name'
-                        className = 'field'
-                        onChange = {this.handleChange}
-                    />
+            <div className="ui centered card">
+                <div className="content">
+                    <form onSubmit={this.handleFormSubmit} className='ui form'>
+                        <label>
+                            Your name: 
+                            <input
+                                type = 'text'    
+                                // value = {this.props.nameValue}
+                                placeholder = 'John'
+                                ref = 'name'
+                                className = 'field'
+                                onChange = {this.handleChange}
+                            />
+                        </label>
 
-                    <input
-                        type = 'submit'
-                        className = 'ui basic blue button'
-                    />
-                </form>
+                        <input
+                            type = 'submit'
+                            className = 'ui basic blue button'
+                        />
+                    </form>
+                </div>
+            </div>
+        );
+    }
+}
+
+class Form3 extends Component
+{  
+    render()
+    {
+        return(
+            <div className="content">
+                <h3>Names: </h3>
+                <ul>
+                    {this.props.names.map((name, i) => 
+                        <li key={i}>{name}</li>
+                    )}
+                </ul>
             </div>
         );
     }
